5f6c85fb7a5f16a87316206e1940eb316d5201ae
lparolari <luca.parolari23@gmail.com>
2021-04-14 18:33:41 +0200

    test: add overall test suites check

--- tests/runall.sh
+++ tests/runall.sh
@@ -6,20 +6,35 @@ setenv_debug="${setenv_debug:-0}"
 setenv_verbose="${setenv_verbose:-1}"
 
 runall() {
+    ok="${tt}"
+
     utils_suite="utils.test.sh"
     utils_tests=("it_prints_debug_strings_if_enabled" "it_does_not_print_debug_strings_when_disabled" "it_returns_true_if_file_exists" "it_returns_false_if_file_does_not_exists" "it_returns_true_when_user_wants_to_proceed")
 
     runsuite "${utils_suite}" "${utils_tests[@]}"
+    [ "$?" == "${ff}" ] && ok="${ff}"
 
     setenv_suite="setenv.test.sh"
     setenv_tests=("it_symlinks_all_content_in_folder" "it_fails_creating_symlinks_if_no_defaults")
 
     runsuite "${setenv_suite}" "${setenv_tests[@]}"
+    [ "$?" == "${ff}" ] && ok="${ff}"
 
     backup_suite="backup.test.sh"
     backup_tests=("it_creates_targz_backups")
 
     runsuite "${backup_suite}" "${backup_tests[@]}"
+    [ "$?" == "${ff}" ] && ok="${ff}"
+
+    echo "----------------------------------------------------------------------"
+
+    if [ "${ok}" = "${tt}" ]; then
+        echo "OK"
+        return "${tt}"
+    else
+        echo "Completed with errors!"
+        return "${ff}"
+    fi
 }
 
 runsuite() {
@@ -56,6 +71,7 @@ runsuite() {
     if [ "${n_failed}" -gt "0" ]; then
         return 1
     fi
+    return 0
 }
 
 runall
